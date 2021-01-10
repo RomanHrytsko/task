aba01e10fd56cc806ad5a6f6b60ebace2d072c53
Roman Hrytsko <72077761+RomanHrytsko@users.noreply.github.com>
2021-01-10 21:26:14 +0200

    Delete userPostsService.js

--- src/services/userPostsService.js
+++
@@ -1,9 +0,0 @@
-export class UserPostsService{
-    constructor(httpClient) {
-        this.api = httpClient
-        this.baseUrl = 'https://jsonplaceholder.typicode.com/posts?userId='
-    }
-    getUserPosts(id){
-        return this.api.get(this.baseUrl+id)
-    }
-}
